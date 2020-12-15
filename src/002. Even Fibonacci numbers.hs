8bf638ebfc09a5674daaeabe613320a8c5caea88
Tungdji <wtd53pm@outlook.com>
2020-12-15 18:30:25 +0800

    Finish 002

--- src/002. Even Fibonacci numbers.hs
+++ src/002. Even Fibonacci numbers.hs
@@ -1,11 +1,12 @@
-main :: IO ()
+module Main where
 
-main = do
-    putStrLn $ show result where
-        result = sum [ x | x <- fibSeq, x < 4000000, even x ] where
-            //TODO
-            //The function doesn't know that fibSeq is an incremental sequence, so won't stop at 4000000
-            fibSeq = [ fib x | x <- [1..] ] where
-                fib 1 = 1
-                fib 2 = 2
-                fib n = fib (n - 1) + fib (n - 2)
+    main :: IO ()
+
+    main = do
+        putStrLn $ show result where
+            result = sum $ takeWhile (< 4000000) [ x | x <- fibSeq, even x ] where
+                fibSeq = [ fib x | x <- [1..] ] where
+                    fib 1 = 1
+                    fib 2 = 2
+                    fib n = fib (n - 1) + fib (n - 2)
+        
