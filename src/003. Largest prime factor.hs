5d9c6cb7ae359d0e0e0bf20c8f74b1721c490d35
Tungdji <wtd53pm@outlook.com>
2020-12-16 13:56:57 +0800

    Solve 003

--- src/003. Largest prime factor.hs
+++ src/003. Largest prime factor.hs
@@ -1,8 +1,9 @@
 module Main where
 
+    import Utils
+
     main :: IO ()
 
     main = do
-        putStrLn $ show theLargestPrimeFactorOf 600851475143 where
-            theLargestPrimeFactorOf n =
+        putStrLn $ show $ maximum $ [ x | x <- [1 .. floor $ sqrt 600851475143], mod 600851475143 x == 0, prime x ]
         
